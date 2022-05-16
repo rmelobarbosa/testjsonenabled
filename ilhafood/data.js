497b0c9f6ec6a21411e23bd008a8fe7606df1812
Rafael Barbosa <rmelobarbosa@gmail.com>
2022-05-16 20:52:06 -0300

    Add files via upload

---
+++ ilhafood/data.js
@@ -0,0 +1,32 @@
+// ALTERAVEL
+export default {
+  "title": "Ilha Food Park",
+  "bannerUrlDesktop": "https://prdmktplace.z15.web.core.windows.net/ilhafoodpark/banner-ilha-food-park.png",
+  "bannerUrlMobile": "https://prdmktplace.z15.web.core.windows.net/ilhafoodpark/banner-ilha-food-park-mobile.png",
+  "organizations": [
+   {
+      "tags": ["restaurante"],
+      "suffix": "oxentemix"
+    },
+    {
+      "tags": ["hotdog"],
+      "suffix": "catioroaracagy"
+    },
+    {
+      "tags": ["hotdog", "hamburguer"],
+      "suffix": "drsandubaaracagy"
+    },
+    {
+      "tags": ["pizza, massas"],
+      "suffix": "pizzasunset"
+    },
+    {
+      "tags": ["pastel"],
+      "suffix": "pastelandiaslz"
+    },
+	{
+      "tags": ["massas"],
+      "suffix": "nationsfoodslz"
+    }
+  ]
+}
