73f3fe7faa9517b2c9e66d43551f535fdcf5fc3c
Rafael Barbosa <rmelobarbosa@gmail.com>
2022-07-06 22:01:23 -0300

    commit e ae sio food

--- data.js
+++ data.js
@@ -1,32 +1,140 @@
 // ALTERAVEL
 export default {
-  "title": "Ilha Food Park",
-  "bannerUrlDesktop": "https://rmelobarbosa.github.io/testjsonenabled/banner-ilha-food-park.png",
-  "bannerUrlMobile": "https://rmelobarbosa.github.io/testjsonenabled/banner-ilha-food-park-mobile.png",
+  "title": "E ae Siô Food",
+  "bannerUrlDesktop": "https://rmelobarbosa.github.io/testjsonenabled/banner-eaesio.png",
+  "bannerUrlMobile": "https://rmelobarbosa.github.io/testjsonenabled/banner-eaesio-mobile.png",
   "organizations": [
    {
-      "tags": ["refeição"],
-      "suffix": "oxentemix"
-    },
-    {
-      "tags": ["hotdog"],
-      "suffix": "catioroaracagy"
-    },
-    {
-      "tags": ["hamburguer"],
-      "suffix": "drsandubaaracagy"
-    },
-    {
-      "tags": ["pizza"],
-      "suffix": "pizzasunset"
-    },
-    {
-      "tags": ["pastel"],
-      "suffix": "pastelandiaslz"
+      "tags": ["Pizza"],
+      "suffix": "ladivinasaoluis"
     },
 	{
-      "tags": ["massas"],
-      "suffix": "nationsfoodslz"
+		  "tags": ["Churrasco"],
+		  "suffix": "churrascodovaqueiro"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "revisburger"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "burguerpark"
+		},
+	{
+		  "tags": ["Pizza"],
+		  "suffix": "pizzasunset"
+		},
+	{
+		  "tags": ["Comida Brasileira"],
+		  "suffix": "coisasdopara"
+		},
+	{
+		  "tags": ["Churrasco"],
+		  "suffix": "espetinhoacasaenossa"
+		},
+	{
+		  "tags": ["Churrasco"],
+		  "suffix": "santobraseiro"
+		},
+	{
+		  "tags": ["Massas"],
+		  "suffix": "nationsfoodslz"
+		},
+	{
+		  "tags": ["Churrasco"],
+		  "suffix": "harasbbq"
+		},
+	{
+		  "tags": ["Pastel"],
+		  "suffix": "pastelandiaslz"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "fireburgerslz"
+		},
+	{
+		  "tags": ["Pastel"],
+		  "suffix": "pastelandiabequimao"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "pepeburger"
+		},
+	{
+		  "tags": ["Churrasco"],
+		  "suffix": "casabraseiro"
+		},
+	{
+		  "tags": ["Hot Dog"],
+		  "suffix": "catioro"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "bengburger"
+		},
+	{
+		  "tags": ["Hot Dog"],
+		  "suffix": "catioroaracagy"
+		},
+	{
+		  "tags": ["Comida Brasileira"],
+		  "suffix": "rangosgrill"
+		},
+	{
+		  "tags": ["Comida Brasileira"],
+		  "suffix": "restaurantemocotofeijoadadoedy"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "burguernosso"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "eats_burguer"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "skinaburger"
+		},
+	{
+		  "tags": ["Pizza"],
+		  "suffix": "fugazgourmetpizzaria"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "frazaoburgerhouseslz"
+		},
+	{
+		  "tags": ["Comida Brasileira"],
+		  "suffix": "coisasdopara"
+		},
+	{
+		  "tags": ["Pizza"],
+		  "suffix": "diggiopizza"
+		},
+	{
+		  "tags": ["Churrasco"],
+		  "suffix": "fogobovino_"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "chefburgerslz"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "rota50burger"
+		},
+	{
+		  "tags": ["Hamburgueria"],
+		  "suffix": "sousasburger"
+		},
+	{
+		  "tags": ["Churrasco"],
+		  "suffix": "oxentemix"
+		},
+	{
+      "tags": ["Tapioca"],
+      "suffix": "tarochedatapiocaria"
     }
   ]
 }
