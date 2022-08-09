8577c3540a6c8a73919a61aaa3564daa6235dc03
Rafael Barbosa <rmelobarbosa@gmail.com>
2022-08-09 19:32:30 -0300

    atualizacao

--- data.js
+++ data.js
@@ -132,6 +132,10 @@ export default {
 	{
       "tags": ["Tapioca"],
       "suffix": "tarochedatapiocaria"
+    },
+    {
+      "tags": ["Sushi"],
+      "suffix": "reidosushicohatrac1"
     }
   ]
 }
